ca94d64a7e925c479b976322a6e17da4978f6a88
Stefan Maties <tools@maties.dev>
2023-05-26 08:37:09 +0200

    Fetch moved inside function

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,49 +1,53 @@
 <script>
   import ContentBox from './ContentBox.svelte';
-
   import * as cheerio from 'cheerio';
+
+  let showBlog = false;
 
   const sheetUrl =
     'https://docs.google.com/spreadsheets/d/1bsScW5hnMQus6CXvMDCcjMQ6q4iBqZINbmD3rekBFnc/edit#gid=0';
   let filteredData = [];
-  fetch(sheetUrl)
-    .then((response) => response.text())
-    .then((data) => {
-      const $ = cheerio.load(data);
-      const rows = $('table tbody tr');
-      rows.each((index, row) => {
-        const id = $(row).find('td:nth-child(2)').text();
-        const headerText = $(row).find('td:nth-child(3)').text();
-        const content = $(row).find('td:nth-child(4)').text();
-        const description = $(row).find('td:nth-child(5)').text();
-        const rawImg = $(row).find('td:nth-child(6)').text();
-        let img;
-        if (rawImg !== '') {
-          const rawImgPre = rawImg
-            .split('')
-            .splice(32)
-            .join('')
-            .split('')
-            .splice(0, 33)
-            .join('');
-          img = `https://drive.google.com/uc?export=download&id=${rawImgPre}`;
-        }
-        // if (rawImg == '') {
-        //   img =
-        //     'https://drive.google.com/uc?export=download&id=1q12Svt00-gmkhdLEmTmLW3zprRbxHwaZ';
-        // }
-        filteredData.push({ id, headerText, content, description, img });
-      });
-      filteredData = filteredData;
-      console.log(filteredData);
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-    });
 
-  let showBlog = false;
+  function fetchPosts() {
+    fetch(sheetUrl)
+      .then((response) => response.text())
+      .then((data) => {
+        const $ = cheerio.load(data);
+        const rows = $('table tbody tr');
+        rows.each((index, row) => {
+          const id = $(row).find('td:nth-child(2)').text();
+          const headerText = $(row).find('td:nth-child(3)').text();
+          const content = $(row).find('td:nth-child(4)').text();
+          const description = $(row).find('td:nth-child(5)').text();
+          const rawImg = $(row).find('td:nth-child(6)').text();
+          let img;
+          if (rawImg !== '') {
+            const rawImgPre = rawImg
+              .split('')
+              .splice(32)
+              .join('')
+              .split('')
+              .splice(0, 33)
+              .join('');
+            img = `https://drive.google.com/uc?export=download&id=${rawImgPre}`;
+          }
+          // if (rawImg == '') {
+          //   img =
+          //     'https://drive.google.com/uc?export=download&id=1q12Svt00-gmkhdLEmTmLW3zprRbxHwaZ';
+          // }
+          filteredData.push({ id, headerText, content, description, img });
+        });
+        filteredData = filteredData;
+        // console.log(filteredData);
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  }
+
   function viewBlog() {
     showBlog = true;
+    fetchPosts();
   }
 </script>
 
@@ -70,7 +74,7 @@
       /></g
     ></svg
   >
-  <button on:click={viewBlog}>View posts</button>
+  <button on:click={viewBlog}>View Blog posts</button>
 </ContentBox>
 
 {#if showBlog}
